afb002ad194f45248bcb3029272473aa398572a7
mohammed <mohammedsherif675@gamil.com>
2025-02-18 11:57:23 +0200

    i library

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    screens: {
+      sm: '370px',
+      sml:'500px',
+      md: '667px',
+      mdl: '778px',
+      lg: '1024px',
+      xl: '1280px',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
